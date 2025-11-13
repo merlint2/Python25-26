c652b346f7093c91e6a530eccf426ede1c9891ba
merlint2 <merlint2@nycstudents.net>
2025-11-13 10:49:42 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,25 @@
+from myfunctions import *
+
+bob.speed(0)
+turtle.bgcolor("black")
+
+bob.color("red")
+
+bob.fillcolor(color)
+bob.begin_fill()
+
+bob.color("red", c)
+c = (255,)
+
+
+
+bob.color("red")
+
+
+
+bob.penup()
+bob.goto(-16,100)
+bob.right(25)
+
+
+bob.pendown()
---
+++ myFunctions.py
@@ -0,0 +1,158 @@
+import turtle
+bob = turtle.Turtle()
+
+bob.speed(0)
+
+turtle.bgcolor("black")
+
+bob.color("red")
+
+bob.pensize(3)
+
+
+for times in range(20):
+    
+    bob.forward(50)
+    bob.left(72)
+    bob.forward(50)
+    bob.left(72)
+    bob.forward(50)
+    bob.left(72)
+    bob.forward(50)
+    bob.left(72)
+    bob.forward(50)
+    bob.left(72)
+
+    bob.left(20)
+
+
+bob.color("pink")
+
+for times in range(20):
+    bob.forward(80)
+    bob.left(72)
+    bob.forward(80)
+    bob.left(72)
+    bob.forward(80)
+    bob.left(72)
+    bob.forward(80)
+    bob.left(72)
+    bob.forward(80)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color("purple")
+
+for times in range(20):
+    bob.forward(100)
+    bob.left(72)
+    bob.forward(100)
+    bob.left(72)
+    bob.forward(100)
+    bob.left(72)
+    bob.forward(100)
+    bob.left(72)
+    bob.forward(100)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color("blue")
+
+for times in range(20):
+    bob.forward(125)
+    bob.left(72)
+    bob.forward(125)
+    bob.left(72)
+    bob.forward(125)
+    bob.left(72)
+    bob.forward(125)
+    bob.left(72)
+    bob.forward(125)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color("red")
+
+for times in range(20):
+    bob.forward(150)
+    bob.left(72)
+    bob.forward(150)
+    bob.left(72)
+    bob.forward(150)
+    bob.left(72)
+    bob.forward(150)
+    bob.left(72)
+    bob.forward(150)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color ("pink")
+
+for times in range(20):
+    bob.forward(175)
+    bob.left(72)
+    bob.forward(175)
+    bob.left(72)
+    bob.forward(175)
+    bob.left(72)
+    bob.forward(175)
+    bob.left(72)
+    bob.forward(175)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color("purple")
+
+for times in range(20):
+    bob.forward(200)
+    bob.left(72)
+    bob.forward(200)
+    bob.left(72)
+    bob.forward(200)
+    bob.left(72)
+    bob.forward(200)
+    bob.left(72)
+    bob.forward(200)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color("blue")
+
+for times in range(20):
+    bob.forward(250)
+    bob.left(72)
+    bob.forward(250)
+    bob.left(72)
+    bob.forward(250)
+    bob.left(72)
+    bob.forward(250)
+    bob.left(72)
+    bob.forward(250)
+    bob.left(72)
+
+    bob.left(20)
+
+bob.color("white")
+
+bob.pensize(1.5)
+
+for times in range(20):
+    bob.forward(300)
+    bob.left(72)
+    bob.forward(300)
+    bob.left(72)
+    bob.forward(300)
+    bob.left(72)
+    bob.forward(300)
+    bob.left(72)
+    bob.forward(300)
+    bob.left(72)
+
+    bob.left(20)
+
